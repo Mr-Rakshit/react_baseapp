c698663fef97b78584e288fba2c8f15e31addd1c
Mr-Rakshit <rakshitsinghoff@gmail.com>
2021-06-30 09:41:22 +0530

    removed unwanted code , running sweet af

--- client/src/Display.js
+++ client/src/Display.js
@@ -38,110 +38,18 @@ return (
          <span className='date'>Joined in 2015</span>
        </Card.Meta>
        <Card.Description>
-         Matthew is a musician living in Nashville.
+         {curr.last_name}
        </Card.Description>
      </Card.Content>
      <Card.Content extra>
        <a>
          <Icon name='user' />
-         22 Friends
+         {curr.email}
        </a>
      </Card.Content>
    </Card>
      )
    })
-  //  <div className="Cardall">
-  //  <Card className="Card">
-  //   <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-  //   <Card.Content>
-  //     <Card.Header>Matthew</Card.Header>
-  //     <Card.Meta>
-  //       <span className='date'>Joined in 2015</span>
-  //     </Card.Meta>
-  //     <Card.Description>
-  //       Matthew is a musician living in Nashville.
-  //     </Card.Description>
-  //   </Card.Content>
-  //   <Card.Content extra>
-  //     <a>
-  //       <Icon name='user' />
-  //       22 Friends
-  //     </a>
-  //   </Card.Content>
-  // </Card>
-  // <Card className="Card">
-  //   <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-  //   <Card.Content>
-  //     <Card.Header>Matthew</Card.Header>
-  //     <Card.Meta>
-  //       <span className='date'>Joined in 2015</span>
-  //     </Card.Meta>
-  //     <Card.Description>
-  //       Matthew is a musician living in Nashville.
-  //     </Card.Description>
-  //   </Card.Content>
-  //   <Card.Content extra>
-  //     <a>
-  //       <Icon name='user' />
-  //       22 Friends
-  //     </a>
-  //   </Card.Content>
-  // </Card>
-  // <Card className="Card">
-  //   <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-  //   <Card.Content>
-  //     <Card.Header>Matthew</Card.Header>
-  //     <Card.Meta>
-  //       <span className='date'>Joined in 2015</span>
-  //     </Card.Meta>
-  //     <Card.Description>
-  //       Matthew is a musician living in Nashville.
-  //     </Card.Description>
-  //   </Card.Content>
-  //   <Card.Content extra>
-  //     <a>
-  //       <Icon name='user' />
-  //       22 Friends
-  //     </a>
-  //   </Card.Content>
-  // </Card>
-  // <Card className="Card">
-  //   <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-  //   <Card.Content>
-  //     <Card.Header>Matthew</Card.Header>
-  //     <Card.Meta>
-  //       <span className='date'>Joined in 2015</span>
-  //     </Card.Meta>
-  //     <Card.Description>
-  //       Matthew is a musician living in Nashville.
-  //     </Card.Description>
-  //   </Card.Content>
-  //   <Card.Content extra>
-  //     <a>
-  //       <Icon name='user' />
-  //       22 Friends
-  //     </a>
-  //   </Card.Content>
-  // </Card>
-  // <Card className="Card">
-  //   <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-  //   <Card.Content>
-  //     <Card.Header>Matthew</Card.Header>
-  //     <Card.Meta>
-  //       <span className='date'>Joined in 2015</span>
-  //     </Card.Meta>
-  //     <Card.Description>
-  //       Matthew is a musician living in Nashville.
-  //     </Card.Description>
-  //   </Card.Content>
-  //   <Card.Content extra>
-  //     <a>
-  //       <Icon name='user' />
-  //       22 Friends
-  //     </a>
-  //   </Card.Content>
-  // </Card>
-  //  </div> 
 )
 }
 
