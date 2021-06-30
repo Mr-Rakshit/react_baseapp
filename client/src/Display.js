3be6eb27d7acfd7b7909626dc97a06f091c932ee
Mr-Rakshit <rakshitsinghoff@gmail.com>
2021-06-30 09:38:28 +0530

    map is working just fine

--- client/src/Display.js
+++ client/src/Display.js
@@ -6,7 +6,7 @@ function CardExampleCard() {
   const [data,setData] = useState([])
   useEffect(()=> {
     async function fetchData() {
-      console.log("kuch hua?")
+
        fetch('/display',{
         headers : { 
           'Content-Type': 'application/json',
@@ -19,104 +19,129 @@ function CardExampleCard() {
             console.log(json);
             setData(json)
        });
-     }
+
+       }
+
+
     fetchData();
    }, []);
   
 
 return (
-   <div className="Cardall">
-   <Card className="Card">
-    <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>Matthew</Card.Header>
-      <Card.Meta>
-        <span className='date'>Joined in 2015</span>
-      </Card.Meta>
-      <Card.Description>
-        Matthew is a musician living in Nashville.
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        22 Friends
-      </a>
-    </Card.Content>
-  </Card>
-  <Card className="Card">
-    <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>Matthew</Card.Header>
-      <Card.Meta>
-        <span className='date'>Joined in 2015</span>
-      </Card.Meta>
-      <Card.Description>
-        Matthew is a musician living in Nashville.
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        22 Friends
-      </a>
-    </Card.Content>
-  </Card>
-  <Card className="Card">
-    <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>Matthew</Card.Header>
-      <Card.Meta>
-        <span className='date'>Joined in 2015</span>
-      </Card.Meta>
-      <Card.Description>
-        Matthew is a musician living in Nashville.
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        22 Friends
-      </a>
-    </Card.Content>
-  </Card>
-  <Card className="Card">
-    <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>Matthew</Card.Header>
-      <Card.Meta>
-        <span className='date'>Joined in 2015</span>
-      </Card.Meta>
-      <Card.Description>
-        Matthew is a musician living in Nashville.
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        22 Friends
-      </a>
-    </Card.Content>
-  </Card>
-  <Card className="Card">
-    <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>Matthew</Card.Header>
-      <Card.Meta>
-        <span className='date'>Joined in 2015</span>
-      </Card.Meta>
-      <Card.Description>
-        Matthew is a musician living in Nashville.
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        22 Friends
-      </a>
-    </Card.Content>
-  </Card>
-   </div> 
+   data.map((curr) => {
+     return(
+    <Card className="Card">
+     <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+     <Card.Content>
+       <Card.Header>{curr.first_name}</Card.Header>
+       <Card.Meta>
+         <span className='date'>Joined in 2015</span>
+       </Card.Meta>
+       <Card.Description>
+         Matthew is a musician living in Nashville.
+       </Card.Description>
+     </Card.Content>
+     <Card.Content extra>
+       <a>
+         <Icon name='user' />
+         22 Friends
+       </a>
+     </Card.Content>
+   </Card>
+     )
+   })
+  //  <div className="Cardall">
+  //  <Card className="Card">
+  //   <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+  //   <Card.Content>
+  //     <Card.Header>Matthew</Card.Header>
+  //     <Card.Meta>
+  //       <span className='date'>Joined in 2015</span>
+  //     </Card.Meta>
+  //     <Card.Description>
+  //       Matthew is a musician living in Nashville.
+  //     </Card.Description>
+  //   </Card.Content>
+  //   <Card.Content extra>
+  //     <a>
+  //       <Icon name='user' />
+  //       22 Friends
+  //     </a>
+  //   </Card.Content>
+  // </Card>
+  // <Card className="Card">
+  //   <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+  //   <Card.Content>
+  //     <Card.Header>Matthew</Card.Header>
+  //     <Card.Meta>
+  //       <span className='date'>Joined in 2015</span>
+  //     </Card.Meta>
+  //     <Card.Description>
+  //       Matthew is a musician living in Nashville.
+  //     </Card.Description>
+  //   </Card.Content>
+  //   <Card.Content extra>
+  //     <a>
+  //       <Icon name='user' />
+  //       22 Friends
+  //     </a>
+  //   </Card.Content>
+  // </Card>
+  // <Card className="Card">
+  //   <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+  //   <Card.Content>
+  //     <Card.Header>Matthew</Card.Header>
+  //     <Card.Meta>
+  //       <span className='date'>Joined in 2015</span>
+  //     </Card.Meta>
+  //     <Card.Description>
+  //       Matthew is a musician living in Nashville.
+  //     </Card.Description>
+  //   </Card.Content>
+  //   <Card.Content extra>
+  //     <a>
+  //       <Icon name='user' />
+  //       22 Friends
+  //     </a>
+  //   </Card.Content>
+  // </Card>
+  // <Card className="Card">
+  //   <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+  //   <Card.Content>
+  //     <Card.Header>Matthew</Card.Header>
+  //     <Card.Meta>
+  //       <span className='date'>Joined in 2015</span>
+  //     </Card.Meta>
+  //     <Card.Description>
+  //       Matthew is a musician living in Nashville.
+  //     </Card.Description>
+  //   </Card.Content>
+  //   <Card.Content extra>
+  //     <a>
+  //       <Icon name='user' />
+  //       22 Friends
+  //     </a>
+  //   </Card.Content>
+  // </Card>
+  // <Card className="Card">
+  //   <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+  //   <Card.Content>
+  //     <Card.Header>Matthew</Card.Header>
+  //     <Card.Meta>
+  //       <span className='date'>Joined in 2015</span>
+  //     </Card.Meta>
+  //     <Card.Description>
+  //       Matthew is a musician living in Nashville.
+  //     </Card.Description>
+  //   </Card.Content>
+  //   <Card.Content extra>
+  //     <a>
+  //       <Icon name='user' />
+  //       22 Friends
+  //     </a>
+  //   </Card.Content>
+  // </Card>
+  //  </div> 
 )
 }
 
